9a768e3070938658c0d6961a80d5a2f17f0fbfdf
Ben Griffiths <zp21899@bristol.ac.uk>
2025-01-10 09:56:37 +0000

    Folder for own testing. Don't push to main version of qgam.

---
+++ ben/test_new_elf.R
@@ -0,0 +1,41 @@
+# install on master
+# load_all on new_elf
+
+# use cv.qgam::qgam_cv for original elf
+# use qgam::qgam for new elf
+
+set.seed(4)
+n <- 1e4
+qu <- 0.1
+
+dat <- mgcv::gamSim(1,n=n,dist="normal",scale=10)
+form <- y ~ s(x0) + s(x1) + s(x2) + s(x3)
+
+fit.elf <- qgam.old::qgam(form = form, data = dat, qu = qu, discrete = TRUE)
+fit.nelf <- qgam::qgam(form = form, data = dat, qu = qu, discrete = TRUE)
+
+pinLoss(dat$y, fit.elf$fitted.values, qu = qu)
+pinLoss(dat$y, fit.nelf$fitted.values, qu = qu)
+
+plot(fit.elf$fitted.values, fit.nelf$fitted.values)
+abline(0,1,col=2)
+
+c(fit.elf$aic, fit.nelf$aic)
+c(fit.elf$family$getTheta(), fit.nelf$family$getTheta())
+c(fit.elf$family$getCo(), fit.nelf$family$getCo())
+
+
+dat$y <- dat$y * ((dat$x2) * 3 / 5 + 0.7)
+form <- list(y ~ s(x0) + s(x1) + s(x2) + s(x3), ~ s(x2))
+
+fit.elf <- qgam.old::qgam(form = form, data = dat, qu = qu, discrete = TRUE)
+fit.nelf <- qgam::qgam(form = form, data = dat, qu = qu, discrete = TRUE)
+
+pinLoss(dat$y, fit.elf$fitted.values, qu = qu)
+pinLoss(dat$y, fit.nelf$fitted.values, qu = qu)
+
+plot(fit.elf$fitted.values, fit.nelf$fitted.values)
+abline(0,1,col=2)
+
+fit.elf$aic
+fit.nelf$aic
